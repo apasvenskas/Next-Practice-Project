551a928c0389bd311dc6b402c571ad72894b8a4b
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-03-03 10:23:05 -0800

    added css to events

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import classes from '@/styles/event-item.module.css'
 
 function EventItem(props){
     const { title, image, date, location, id } = props
@@ -15,20 +16,20 @@ function EventItem(props){
     const exploreLink = `/events/${id}`;
 
     return(
-        <li>
+        <li className={classes.item} >
             {/* getting the desired image from public image folder */}
             <img srv={'/' + image} alt={title} />
-            <div>
-                <div>
+            <div className={classes.content}>
+                <div className="classes.summary">
                     <h2>{title}</h2>
-                    <div>
+                    <div className={classes.date}>
                     <time>{humanReadableDate}</time>
                     </div>
-                    <div>
+                    <div className={classes.address}>
                     <address>{formattedAddress}</address>
                     </div>
                 </div>
-                <div>
+                <div className={classes.actions}>
                     <Link href={exploreLink}>Expolre Event</Link>
                 </div>
             </div>
