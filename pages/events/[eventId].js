ddf0e91f7ff4f6b8b02a9d3d783461536f12d119
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-03-07 16:25:23 -0800

    header added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,33 @@
+import EventContent from "@/components/events/event-detail/event-content";
+import EventLogistics from "@/components/events/event-detail/event-logistics";
+import EventSummary from "@/components/events/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 function EventDetailPage(){
+    const router = useRouter();
+    const eventId = router.query.eventId; 
+    const event = getEventById(eventId);
+
+    if(!event){
+        return <p>No event found!</p>
+    }
+
     return (
         <div>
-            <h1>Event Detail</h1>
+            <Fragment>
+                <EventSummary title={event.title}/>
+                <EventLogistics 
+                    date={event.date} 
+                    adress={event.location} 
+                    image={event.image} 
+                    imageAlt={event.title}
+                />
+                <EventContent>
+                    <p>{event.description}</p>
+                </EventContent>
+            </Fragment>
         </div>
     )
 }
