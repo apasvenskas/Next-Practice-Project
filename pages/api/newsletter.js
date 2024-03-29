db52cbc65b0e8871b3c528a6b524e1dd669e66d0
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-03-29 14:46:24 -0700

    error handling for newsletter complete

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,17 @@
 import { MongoClient } from "mongodb";
 
+async function connect(){
+    const client = await MongoClient.connect
+        ('mongodb+srv://audrius13toto:Kaskos1234@nextjs.vdn1m1j.mongodb.net/newsletter?retryWrites=true&w=majority&appName=NextJS')
+        return client;
+}
+
+async function insert(client, document){
+    const db = client.db();
+
+    await db.collection('newsletter').insertOne(document); 
+}
+
 async function handler(req, res){
     if(req.method === 'POST'){ // only proceed if it is a post request
         console.log('Received body:', req.body);
@@ -10,11 +22,22 @@ async function handler(req, res){
             return;
         }
 
-        const client = await MongoClient.connect
-        ('mongodb+srv://audrius13toto:Kaskos1234@nextjs.vdn1m1j.mongodb.net/newsletter?retryWrites=true&w=majority&appName=NextJS')
-            const db = client.db();
-            await db.collection('emails').insertOne({email: userEmail})
+        let client; 
+
+        // updated the function with connect and insert
+        try {
+            client = await connect();
+        } catch (error) {
+            res.status(500).json({message: 'Connecting to the database failed!'});
+            return;
+        }
+        try {
+            await insert(client, {email: userEmail})
             client.close();
+        } catch(error){
+            res.status(500).json({message: 'Inserting data failed!'});
+            return;
+        }
         console.log(userEmail);
         res.status(201).json({ message: 'Signed up!' })
     }
