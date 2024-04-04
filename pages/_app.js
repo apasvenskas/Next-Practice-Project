77d3be9f181212c8de65a37019ec86a81c67c192
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-04-04 14:57:37 -0700

    added Notifications to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Notification from '../components/ui/notification'
 import Layout from "@/components/layout/layout";
 import "../styles/globals.css";
 // this file _app.js is global file what goes here will be available on all the pages
@@ -7,9 +8,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='description' content="Next.js Events" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test" status="pending"/>
     </Layout>
   );
 }
